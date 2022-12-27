213d1afa616e3c686ffcb23a8f82277dcc6730fe
Ashley Ung <62251927+ung8005@users.noreply.github.com>
2022-12-27 05:20:57 -0800

    alarm program

---
+++ alarm.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+alarm.py
+Ashley Ung 
+
+This program functions as an alarm. It will ask the user for an alarm time and then set the alarm to play the alarm sound when the current time matches the alarm time.  
+"""
+
+from playsound import playsound
+import time
+import threading
+import pygame
+
+# Function to play the alarm sound. Loads the alarm sound, this file named "alarm.mp3", is downloaded from an external source
+def playAlarm (alarmTime):
+	pygame.mixer.init()
+	pygame.mixer.music.load ("alarm.mp3")
+	while True:
+		currentTime = time.strftime ("%H:%M")
+		if currentTime == alarmTime:
+			print ("Wake up and have a great day!")
+			playsound ("alarm.mp3")
+			break
+		
+# Obtain the alarm time from the user
+alarmTime = input ("Enter the time for the alarm (HH:MM): ") # Uses military time notation
+
+
+print ("Alarm is set for " + alarmTime)
